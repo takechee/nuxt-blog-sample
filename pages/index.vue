7beb588d7b2bddd1d33b2c72a8858aec628f1f3c
takechee <takechee14@gmail.com>
2020-12-03 20:59:43 +0900

    コンテンツを追加

--- pages/index.vue
+++ pages/index.vue
@@ -3,25 +3,26 @@
     <section class="hero is-medium is-primary is-bold">
       <div class="hero-body">
         <div class="container">
-          <h1 class="title">
-            Welcome to the JavaScript SSR Blog.
-          </h1>
-          <h2 class="subtitle">
-            Hope you find something you like.
-          </h2>
+          <h1 class="title">Welcome to the JavaScript SSR Blog.</h1>
+          <h2 class="subtitle">Hope you find something you like.</h2>
         </div>
       </div>
     </section>
+    <Posts />
   </div>
 </template>
 
 <script>
+import Posts from '~/components/Posts.vue'
+
 export default {
   head: {
-    title: 'Home'
-  }
+    title: 'Home',
+  },
+  components: {
+    Posts,
+  },
 }
 </script>
 
-<style>
-</style>
+<style></style>
