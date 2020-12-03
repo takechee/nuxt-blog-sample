04e00ce01e932a7ab1b04bee2b47ebc7a9f80cc5
takechee <takechee14@gmail.com>
2020-12-03 20:31:35 +0900

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <!-- navigation -->
+    <nav class="nav has-shadow">
+      <div class="container">
+        <div class="nav-left">
+          <nuxt-link to="/" class="nav-item">
+            Awesome JS SSR Blog!
+          </nuxt-link>
+          <nuxt-link active-class="is-active" to="/" class="nav-item is-tab" exact>Home</nuxt-link>
+          <nuxt-link active-class="is-active" to="/about" class="nav-item is-tab" exact>About</nuxt-link>
+        </div>
+      </div>
+    </nav>
+    <!-- /navigation -->
+    <!-- displays the page component -->
+    <nuxt/>
+  </div>
+</template>
+<style>
+html {
+  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+*, *:before, *:after {
+  box-sizing: border-box;
+  margin: 0;
+}
+</style>
